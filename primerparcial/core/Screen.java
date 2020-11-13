f5cd6fc6dd0de9f50d063c642cae155cba8152f8
FranzBurneo <franzdavidburneo@gmail.com>
2020-11-12 19:06:14 -0500

    Add files via upload

--- primerparcial/core/Screen.java
+++ primerparcial/core/Screen.java
@@ -1,6 +1,30 @@
 package ec.edu.utpl.arqapl.o20f21.semana5.primerparcial.core;
 
-public abstract class Screen {
+public abstract class CellphoneScreen {
+    private int width;
+    private int height;
+    private int brightness;
+
+    abstract String getType();
+}
+
+public abstract class TabletScreen {
+    private int width;
+    private int height;
+    private int brightness;
+
+    abstract String getType();
+}
+
+public abstract class BraileScreen {
+    private int width;
+    private int height;
+    private int brightness;
+
+    abstract String getType();
+}
+
+public abstract class TVScreen {
     private int width;
     private int height;
     private int brightness;
